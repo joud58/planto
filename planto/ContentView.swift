dd7d0d2524a5af413c27f2a759279a4d369c7a1f
joud fahad <joud_58iii@icloud.com>
2024-10-25 19:43:57 +0300

    Initial Commit

---
+++ planto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  planto
+//
+//  Created by Joud Alhatim on 4/22/46.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ planto/plantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantoApp.swift
+//  planto
+//
+//  Created by Joud Alhatim on 4/22/46.
+//
+
+import SwiftUI
+
+@main
+struct plantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
